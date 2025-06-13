850d9ab3946245a170bd89f94883d4a03678d111
juaniten <juanguijarroferreiro@gmail.com>
2025-06-13 14:21:21 +0200

    add a UDP sender

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	udpAddress, err := net.ResolveUDPAddr("udp", "127.0.0.1:42069")
+	if err != nil {
+		log.Fatalf("Error resolving UDP adress: %v", err)
+	}
+
+	connection, err := net.DialUDP("udp", nil, udpAddress)
+	if err != nil {
+		log.Fatalf("error creating UDP connection: %v\n", err)
+	}
+	defer connection.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print(">")
+		line, err := reader.ReadString(byte('\n'))
+		if err != nil {
+			fmt.Printf("error reading line from standard input: %v\n", err)
+		}
+
+		_, err = connection.Write(([]byte)(line))
+		if err != nil {
+			fmt.Printf("error writing line though UDP connection: %v\n", err)
+		}
+
+	}
+}
